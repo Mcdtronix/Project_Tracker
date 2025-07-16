8b7131966fe259f89376a81fcd6241b6c7548490
macdonald gudo <141135997+Mcdtronix@users.noreply.github.com>
2025-07-16 23:31:55 +0200

    fixed a login issue

--- project_tracker/projects/admin.py
+++ project_tracker/projects/admin.py
@@ -2,7 +2,23 @@ from django.contrib import admin
 from .models import  ProjectCategory, Project
 
 # Register your models here.
-admin.site.register(ProjectCategory),
-admin.site.register(Project),
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_filter = ('name','description')
+    search_fields = ('name', 'description')
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'priority','status', 'start_date','end_date', 'estimated_completion_date')
+    list_filter = ('name','priority','status','start_date','end_date', 'estimated_completion_date')
+    search_fields = ('name', 'priority', 'status')
+
+
+
+
+
+
+
+admin.site.register(ProjectCategory, ProjectCategoryAdmin),
+admin.site.register(Project, ProjectAdmin),
 
 
--- project_tracker/tasks/admin.py
+++ project_tracker/tasks/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from . models import Task
 
 # Register your models here.
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'project', 'priority', 'status', 'start_date', 'due_date', 'completed_date')
+    list_filter = ('title','project', 'priority', 'status', 'start_date', 'due_date', 'completed_date')
+    search_fields = ('name', 'project', 'priority', 'status',)
+
+
+admin.site.register(Task,TaskAdmin)
